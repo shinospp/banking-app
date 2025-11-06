ff2fb9ab2b26a87305b46f4fd50448c0568919d6
shinospp <ppshinos@gmail.com>
2025-11-06 23:39:37 +0530

    Logger added

--- src/main/java/com/spp/banking_app/BankingAppApplication.java
+++ src/main/java/com/spp/banking_app/BankingAppApplication.java
@@ -1,13 +1,17 @@
 package com.spp.banking_app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankingAppApplication {
 
-	//Boot class...
+	static Logger log = LoggerFactory.getLogger(BankingAppApplication.class);
+
 	public static void main(String[] args) {
+		log.info("inside BankingAppApplication......");
 		SpringApplication.run(BankingAppApplication.class, args);
 	}
 
