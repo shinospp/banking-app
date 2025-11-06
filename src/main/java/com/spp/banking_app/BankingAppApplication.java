22a62967ea91e6cd333929c430b4843e15b8659a
shinospp <ppshinos@gmail.com>
2025-11-06 23:18:44 +0530

    Third commit

--- src/main/java/com/spp/banking_app/BankingAppApplication.java
+++ src/main/java/com/spp/banking_app/BankingAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingAppApplication {
 
-	//Boot class
+	//Boot class...
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
 	}
