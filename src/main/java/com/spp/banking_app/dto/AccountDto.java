b80b5dd6379fbd809f89323692dfff7c0b291e80
shinospp <ppshinos@gmail.com>
2025-11-07 12:23:40 +0530

    AccountDto changes

--- src/main/java/com/spp/banking_app/dto/AccountDto.java
+++ src/main/java/com/spp/banking_app/dto/AccountDto.java
@@ -2,8 +2,12 @@ package com.spp.banking_app.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 public class AccountDto {
 
